4bc9a141bff571c1e517f28ae62d0b4f1c9ad95d
GuiTadeu <guitadeu.py@gmail.com>
2018-07-12 11:06:26 -0300

    Trabalhando com Nodemon, Method Override e Body Parser

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+app.use(methodOverride('X-HTTP-Method'));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride('X-Method-Override'));
+app.use(methodOverride('_method'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
 	res.status(201);
-	res.json({'name': 'Guilherme Tadeu', 'email': 'guitadeu.py@gmail.com'});
+	if(req.accepts('text')){
+		res.write('name; email\n');
+		res.write('Guilherme Tadeu; guitadeu.py@gmail.com\n');
+		res.end();
+	} else{
+		res.json({'name': 'Guilherme Tadeu', 'email': 'guitadeu.py@gmail.com'});
+	}	
 });
 
 var server = app.listen(3000, function(){
